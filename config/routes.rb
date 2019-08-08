44bbbf62f6e376be7b9c8a36a0bb8aed901f63e9
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 20:02:53 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
-
-  get '/secret' => 'secrets#show'
+  get '/show', to: 'secrets#show'
+  get '/new', to: 'sessions#new'
+  post '/create', to: 'sessions#create'
+  post '/destroy', to: 'sessions#destroy'
 end
