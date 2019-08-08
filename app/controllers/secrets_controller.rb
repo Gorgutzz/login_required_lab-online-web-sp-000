29362572f1f5f1fb719974579a426e6901dd4883
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 20:01:16 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,7 @@
 class SecretsController < ApplicationController
-  before_action :require_logged_in
-
-  def show
-  end
-
-  private
-
-  def require_logged_in
-    redirect_to '/login' unless current_user
-  end
-
-end
+    def show
+        if !logged_in?
+            redirect_to '/login'
+        end
+    end
+end 
